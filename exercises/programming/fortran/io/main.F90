9becb71fd76204afc5fdb777a0f89170f150bb47
NinaIrmeli <nina.irmeli@gmail.com>
2017-06-29 10:37:55 +0200

    Exercise 6 main file

--- exercises/programming/fortran/io/main.F90
+++ exercises/programming/fortran/io/main.F90
@@ -1,8 +1,11 @@
 program main
   use io
   implicit none
+  character(len=20) :: filename
   real, dimension(:,:), allocatable :: field
-
+  
+  filename='bottle.dat' 
+  
   call read_field(field, 'bottle.dat')
 
   call write_field(field, 0)
