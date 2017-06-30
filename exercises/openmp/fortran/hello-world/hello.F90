5f252d03c1c3631e3cc8067572bf38da0e5ec4d9
NinaIrmeli <NinaIrmeli>
2017-06-30 19:30:58 +0300

    Exercise 6 OpenMP

--- exercises/openmp/fortran/hello-world/hello.F90
+++ exercises/openmp/fortran/hello-world/hello.F90
@@ -1,8 +1,18 @@
 program hello
+  use omp_lib
   implicit none
+  integer :: thrd_id, nthreads
 
   print *, 'Hello world!'
   !$omp parallel
+  nthreads=omp_get_num_threads()
+  thrd_id=omp_get_thread_num()
+  !$omp master
+   write(*,*) 'Number of threads: '
+    write(*,*) nthreads
+  !$omp end master
+  print *,'Thread id'
+  print *, thrd_id
   print *, 'X'
   !$omp end parallel
 
