0c42697c79088dd1951c0abfd1315be99cac8d4a
NinaIrmeli <NinaIrmeli>
2017-07-01 18:16:31 +0300

    Exercise 3a MPI

--- exercises/mpi/fortran/message-chain/skeleton.F90
+++ exercises/mpi/fortran/message-chain/skeleton.F90
@@ -7,13 +7,13 @@ program basic
   integer :: rc, myid, ntasks, count
   integer :: status(MPI_STATUS_SIZE)
   integer :: message(size)
-  integer :: receiveBuffer(size)
+  integer :: recbuffer(size)
 
   real(REAL64) :: t0, t1 
 
   call mpi_init(rc)
   call mpi_comm_rank(MPI_COMM_WORLD, myid, rc)
-  call mpi_comm_sizeMPI_COMM_WORLD, ntasks, rc)
+  call mpi_comm_size(MPI_COMM_WORLD, ntasks, rc)
 
   message = myid
 
@@ -23,6 +23,7 @@ program basic
 
   ! TODO: Send and receive as defined in the assignment
   if ( myid < ntasks-1 ) then
+    call MPI_send(message,size,MPI_integer,myid+1,myid+1,MPI_comm_world,rc)
 
      write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
           ' Sent elements: ',size, &
@@ -30,9 +31,10 @@ program basic
   end if
 
   if ( myid > 0 ) then
+    call MPI_recv(recbuffer,size,MPI_integer,myid,myid,MPI_comm_world,status,rc)
 
      write(*,'(A10,I3,A,I3)') 'Receiver: ', myid, &
-          ' First element: ', receiveBuffer(1)
+          ' First element: ', recbuffer(1)
   end if
 
   ! Finalize measuring the time and print it out
