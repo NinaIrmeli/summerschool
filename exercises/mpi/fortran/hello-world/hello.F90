bfa63db8bc21730882fab0a7a21b32206cef9c8f
NinaIrmeli <NinaIrmeli>
2017-07-01 14:28:35 +0300

    Exercise 1 MPI

--- exercises/mpi/fortran/hello-world/hello.F90
+++ exercises/mpi/fortran/hello-world/hello.F90
@@ -1,9 +1,16 @@
 program hello
+  use mpi
   implicit none
+  integer :: rc
+  integer :: comm,rank,nr
 
-  print *, 'Hello world!'
-  !$omp parallel
-  print *, 'X'
-  !$omp end parallel
+  call MPI_Init(rc)
+  call MPI_Comm_rank(MPI_COMM_WORLD,rank,rc)
+  call MPI_Comm_size(mpi_comm_world,nr,rc)
+  print *, 'Hello world! from rank:' ,rank
+  if (rank.eq.0) print *,'Total nr of ranks: ',nr 
+
+  call MPI_Finalize(rc)
+
 
 end program hello
