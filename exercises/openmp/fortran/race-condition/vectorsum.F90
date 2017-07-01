e131ca904deb779bd3da83966e8f79b470546c32
NinaIrmeli <NinaIrmeli>
2017-07-01 16:28:12 +0300

    Some exercise

--- exercises/openmp/fortran/race-condition/vectorsum.F90
+++ exercises/openmp/fortran/race-condition/vectorsum.F90
@@ -15,8 +15,12 @@ program vectorsum
 
   sum = 0
   ! TODO: Parallelize the computation
-  do i = 1, nx
-     sum = sum + vecA(i)
-  end do
+  ! $omp parallel
+  ! $omp do
+  ! do i = 1, nx
+  !   sum = sum + vecA(i)
+  ! end do
+  ! $omp end do
+  ! $omp end parallel
   write(*,*) 'Sum: ', sum
 end program vectorsum
