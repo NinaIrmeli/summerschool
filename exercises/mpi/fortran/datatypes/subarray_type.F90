615318fa009f140703281384de062bad8b7b7dd3
NinaIrmeli <NinaIrmeli>
2017-07-03 11:58:30 +0300

    Exercise 10b MPI

--- exercises/mpi/fortran/datatypes/subarray_type.F90
+++ exercises/mpi/fortran/datatypes/subarray_type.F90
@@ -7,6 +7,7 @@ program datatype2
   integer, dimension(8,8) :: array
   integer :: rank, ierr
   !TODO: declare variable for block datatype
+  integer :: arraytype
   integer, dimension(2) :: sizes, subsizes, offsets
   integer :: i, j
   
@@ -25,14 +26,24 @@ program datatype2
   end if
 
   !TODO: create a datatype for a subblock [2:5][3:5] of the 8x8 matrix
-  !sizes(1) =
-  !sizes(2) =
-  !subsizes(1) =
-  !subsizes(2) =
-  !offsets(1) = 
-  !offsets(2) = 
+  sizes(1) = 8
+  sizes(2) = 8
+  subsizes(1) = 4
+  subsizes(2) = 3
+  offsets(1) = 2 
+  offsets(2) = 3
+
+  call MPI_Type_create_subarray(2,sizes,subsizes,offsets,MPI_order_fortran,MPI_integer,arraytype,ierr)
+  call MPI_type_commit(arraytype,ierr)
 
   !TODO: send a block of a matrix from rank 0 to rank 1
+
+  if (rank.eq.0) then
+    call MPI_send(array,1,arraytype,1,1,MPI_comm_world,ierr)
+  else if (rank.eq.1) then
+    call MPI_recv(array,1,arraytype,0,1,MPI_comm_world,ierr)
+  end if
+
 
   ! Print out the result
   if (rank == 1) then
@@ -42,7 +53,7 @@ program datatype2
   end if
       
   !TODO: free mpi datatype	
-
+  call MPI_type_free(arraytype,ierr)
   call mpi_finalize(ierr)
 
 end program datatype2
